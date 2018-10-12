56ca732454a08e30914cbc9f7f580451f0bf1589
jakimli <ljking7002@gmail.com>
2018-10-12 17:44:05 +0800

    initialize the project

---
+++ src/main/java/com/github/jakimli/json/schema/validator/Schema.java
@@ -0,0 +1,10 @@
+package com.github.jakimli.json.schema.validator;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+class Schema {
+    static void validate() {
+        assertThat(1, is(1));
+    }
+}
---
+++ src/test/java/com/github/jakimli/json/schema/validator/SchemaTest.java
@@ -0,0 +1,13 @@
+package com.github.jakimli.json.schema.validator;
+
+import org.junit.Test;
+
+import static com.github.jakimli.json.schema.validator.Schema.validate;
+
+public class SchemaTest {
+
+    @Test
+    public void setupProject() {
+        validate();
+    }
+}
