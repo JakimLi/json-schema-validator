e4262bf9e95c4b7017f3269088e86e625d6c60b2
jakimli <ljking7002@gmail.com>
2018-10-13 22:14:15 +0800

    add test for valid object verification

--- src/test/java/com/github/jakimli/json/schema/validator/ValidatorTest.java
+++ src/test/java/com/github/jakimli/json/schema/validator/ValidatorTest.java
@@ -9,10 +9,17 @@ import static com.github.jakimli.json.schema.validator.util.FileUtil.readFile;
 public class ValidatorTest {
 
     @Test(expected = ViolateJsonSchemaException.class)
-    public void should_validate_object() throws Exception {
-
+    public void should_validate_invalid_object() throws Exception {
         String schema = readFile("product.schema.json");
         String instance = readFile("invalid.product.json");
+
+        validate(schema, instance);
+    }
+
+    @Test
+    public void should_validate_valid_object() throws Exception {
+        String schema = readFile("product.schema.json");
+        String instance = readFile("product.json");
 
         validate(schema, instance);
     }
