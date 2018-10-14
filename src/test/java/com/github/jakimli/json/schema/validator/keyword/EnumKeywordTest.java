b6032bede9209d8a491f78868bc65384142fb2bb
jakimli <ljking7002@gmail.com>
2018-10-14 18:13:11 +0800

    add test for valid enum

--- src/test/java/com/github/jakimli/json/schema/validator/keyword/EnumKeywordTest.java
+++ src/test/java/com/github/jakimli/json/schema/validator/keyword/EnumKeywordTest.java
@@ -11,12 +11,19 @@ public class EnumKeywordTest extends TestBase {
 
     @Test
     public void should_validate_enum_with_error() throws Exception {
-
         String schema = readFile("keyword/enum/schema.json");
         String instance = readFile("keyword/enum/invalid.json");
 
         exception.expect(SchemaViolatedException.class);
         exception.expectMessage("must be one of values in enum: [monica, chandler], got: joey");
+        validate(schema, instance);
+    }
+
+    @Test
+    public void should_validate_enum_without_error() throws Exception {
+        String schema = readFile("keyword/enum/schema.json");
+        String instance = readFile("keyword/enum/valid.json");
+
         validate(schema, instance);
     }
 }
