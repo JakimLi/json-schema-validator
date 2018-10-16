97561761402de8493df0361326da8979e4dab264
jakimli <ljking7002@gmail.com>
2018-10-16 16:35:07 +0800

    refacotring

--- src/main/java/com/github/jakimli/json/schema/validator/validation/Schema.java
+++ src/main/java/com/github/jakimli/json/schema/validator/validation/Schema.java
@@ -2,12 +2,12 @@ package com.github.jakimli.json.schema.validator.validation;
 
 import com.alibaba.fastjson.JSONObject;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.github.jakimli.json.schema.validator.validation.Type.type;
-import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 
 public class Schema implements JsonType.Validator {
@@ -22,14 +22,13 @@ public class Schema implements JsonType.Validator {
 
     @Override
     public List<Validation> validate() {
-        List<Validation> validations = newArrayList();
-        validations.addAll(type(this).validate());
-        validations.addAll(subSchema());
-        return validations;
+        return Stream.of(type(this).validate(), typeSpecific())
+                .flatMap(Collection::stream)
+                .collect(toList());
     }
 
-    private List<Validation> subSchema() {
-        return Arrays.stream(JsonType.values())
+    private List<Validation> typeSpecific() {
+        return stream(JsonType.values())
                 .map(t -> t.validator(this))
                 .map(JsonType.Validator::validate)
                 .flatMap(Collection::stream)
