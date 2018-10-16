fbdef89bffc2312c973898c28c467a942fb275d4
jakimli <ljking7002@gmail.com>
2018-10-16 21:33:15 +0800

    refactoring

--- src/main/java/com/github/jakimli/json/schema/validator/keywords/MultipleOf.java
+++ src/main/java/com/github/jakimli/json/schema/validator/keywords/MultipleOf.java
@@ -13,23 +13,29 @@ import static java.math.RoundingMode.UNNECESSARY;
 public class MultipleOf implements Keyword {
 
     @Override
-    public List<Validation> validate(String location, Object value) {
-
+    public List<Validation> validate(String location, Object schema) {
         return newArrayList(assertion(instance -> {
-            if (instance instanceof Integer && value instanceof Integer
-                    && (Integer) instance % (Integer) value != 0) {
-
-                throw violated("expected to be multiple of: " + value + ", got: " + instance);
+            
+            if (bothInteger(schema, instance) && !divisible(schema, instance)) {
+                throw violated("expected to be multiple of: " + schema + ", got: " + instance);
             }
 
             BigDecimal actual = toDecimal(instance);
-            BigDecimal factor = toDecimal(value);
+            BigDecimal factor = toDecimal(schema);
 
             if (actual.divide(factor, UNNECESSARY).stripTrailingZeros().scale() > 0) {
                 throw violated("expected to be multiple of: " + factor + ", got: " + actual);
             }
 
         }).at(location));
+    }
+
+    private boolean divisible(Object value, Object instance) {
+        return (Integer) instance % (Integer) value == 0;
+    }
+
+    private boolean bothInteger(Object value, Object instance) {
+        return instance instanceof Integer && value instanceof Integer;
     }
 
     private BigDecimal toDecimal(Object value) {
