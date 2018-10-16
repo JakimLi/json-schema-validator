a6db6cdc64b5955c7a40ca8c5c6fe44b8a172b6f
jakimli <ljking7002@gmail.com>
2018-10-16 22:50:45 +0800

    add test for succes decimal mulitple of int and vise versa

--- src/test/java/com/github/jakimli/json/schema/validator/keyword/MultipleOfTest.java
+++ src/test/java/com/github/jakimli/json/schema/validator/keyword/MultipleOfTest.java
@@ -47,7 +47,7 @@ public class MultipleOfTest extends TestBase {
     }
 
     @Test
-    public void validate_decimal_multiple_of_integer() throws Exception {
+    public void validate_decimal_multiple_of_integer_fail() throws Exception {
         String schema = readFile("keyword/multipleOf/integer.schema.json");
         String instance = readFile("keyword/multipleOf/11.0.json");
 
@@ -57,12 +57,28 @@ public class MultipleOfTest extends TestBase {
     }
 
     @Test
-    public void validate_integer_multiple_of_decimal() throws Exception {
+    public void validate_decimal_multiple_of_integer_success() throws Exception {
+        String schema = readFile("keyword/multipleOf/integer.schema.json");
+        String instance = readFile("keyword/multipleOf/30.0000.json");
+
+        validate(schema, instance);
+    }
+
+    @Test
+    public void validate_integer_multiple_of_decimal_fail() throws Exception {
         String schema = readFile("keyword/multipleOf/decimal.schema.json");
         String instance = readFile("keyword/multipleOf/11.json");
 
         exception.expect(SchemaViolatedException.class);
         exception.expectMessage("expected to be multiple of: 10.0, got: 11");
+        validate(schema, instance);
+    }
+
+    @Test
+    public void validate_integer_multiple_of_decimal_success() throws Exception {
+        String schema = readFile("keyword/multipleOf/decimal.schema.json");
+        String instance = readFile("keyword/multipleOf/20.json");
+
         validate(schema, instance);
     }
 }
