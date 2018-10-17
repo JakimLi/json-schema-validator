685cc20e959a9f7ac95ff53d77c4ed952b99b4e9
jakimli <ljking7002@gmail.com>
2018-10-17 21:11:24 +0800

    integer equals when maximum

--- src/test/java/com/github/jakimli/json/schema/validator/keyword/mumber/MaximumTest.java
+++ src/test/java/com/github/jakimli/json/schema/validator/keyword/mumber/MaximumTest.java
@@ -10,12 +10,20 @@ import static com.github.jakimli.json.schema.validator.util.FileUtil.readFile;
 public class MaximumTest extends TestBase {
 
     @Test
-    public void integer_max_fail() throws Exception {
+    public void integer_max_but_greater_than() throws Exception {
         String schema = readFile("keyword/maximum/integer.schema.json");
         String instance = readFile("keyword/maximum/11.json");
 
         exception.expect(SchemaViolatedException.class);
         exception.expectMessage("expected to less than or equals to: 10, got: 11");
+        validate(schema, instance);
+    }
+
+    @Test
+    public void integer_max_equal() throws Exception {
+        String schema = readFile("keyword/maximum/integer.schema.json");
+        String instance = readFile("keyword/maximum/10.json");
+
         validate(schema, instance);
     }
 }
