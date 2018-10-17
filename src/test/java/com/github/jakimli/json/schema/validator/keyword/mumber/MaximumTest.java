9e97c4b43ff45f582bf672679b5043b76e35eb51
jakimli <ljking7002@gmail.com>
2018-10-17 21:20:51 +0800

    add test for decimal maximum

--- src/test/java/com/github/jakimli/json/schema/validator/keyword/mumber/MaximumTest.java
+++ src/test/java/com/github/jakimli/json/schema/validator/keyword/mumber/MaximumTest.java
@@ -34,4 +34,30 @@ public class MaximumTest extends TestBase {
 
         validate(schema, instance);
     }
+
+    @Test
+    public void decimal_max_but_greater_than() throws Exception {
+        String schema = readFile("keyword/maximum/decimal.schema.json");
+        String instance = readFile("keyword/maximum/11.05.json");
+
+        exception.expect(SchemaViolatedException.class);
+        exception.expectMessage("expected to less than or equals to: 10.05, got: 11.05");
+        validate(schema, instance);
+    }
+
+    @Test
+    public void decimal_max_equal() throws Exception {
+        String schema = readFile("keyword/maximum/decimal.schema.json");
+        String instance = readFile("keyword/maximum/10.05.json");
+
+        validate(schema, instance);
+    }
+
+    @Test
+    public void decimal_max_less_than() throws Exception {
+        String schema = readFile("keyword/maximum/decimal.schema.json");
+        String instance = readFile("keyword/maximum/9.05.json");
+
+        validate(schema, instance);
+    }
 }
